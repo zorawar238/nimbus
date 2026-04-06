1d5811dfd196bae83a9c5776ed032ce6376bdde6
zorawar238 <nishantkr238@gmail.com>
2026-04-07 01:08:51 +0530

    Allow Render Domain in CORS

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Allow CORS for the Netlify frontend and Localhost
-const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'https://graceful-treacle-8cb142.netlify.app'];
+const allowedOrigins = [
+    'http://localhost:3000', 
+    'http://127.0.0.1:5500', 
+    'http://localhost:5500', 
+    'https://graceful-treacle-8cb142.netlify.app',
+    'https://nimbus-w3fa.onrender.com'
+];
 app.use(cors({
     origin: function(origin, callback) {
         // allow requests with no origin (like mobile apps or curl requests)
